af00d700ab7a6eb84210d4915d99738dea859b59
jamal2026-ui <tpletok827@gmail.com>
2026-04-02 20:56:05 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'dompetku-v1';
+const assets = [
+  '/',
+  'index.html',
+  'https://cdn.tailwindcss.com',
+  'https://cdn.jsdelivr.net/npm/chart.js'
+];
+
+// Install Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Ambil data dari cache jika offline
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
+});
